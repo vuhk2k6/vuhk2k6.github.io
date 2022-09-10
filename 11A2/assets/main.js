2d5503005bd24e4a8548b757b3b3567956de7067
vuhk2k6 <109848918+vuhk2k6@users.noreply.github.com>
2022-09-10 23:17:59 +0700

    up

---
+++ 11A2/assets/main.js
@@ -0,0 +1,420 @@
+const items = document.querySelectorAll(".student__list-item");
+const student0 = document.getElementById("student-0");
+const student1 = document.getElementById("student-1");
+const student2 = document.getElementById("student-2");
+const student3 = document.getElementById("student-3");
+const student4 = document.getElementById("student-4");
+const student5 = document.getElementById("student-5");
+const student6 = document.getElementById("student-6");
+const student7 = document.getElementById("student-7");
+const student8 = document.getElementById("student-8");
+const student9 = document.getElementById("student-9");
+const student10 = document.getElementById("student-10");
+const student11 = document.getElementById("student-11");
+const student12 = document.getElementById("student-12");
+const student13 = document.getElementById("student-13");
+const student14 = document.getElementById("student-14");
+const student15 = document.getElementById("student-15");
+const student16 = document.getElementById("student-16");
+const student17 = document.getElementById("student-17");
+const student18 = document.getElementById("student-18");
+const student19 = document.getElementById("student-19");
+const student20 = document.getElementById("student-20");
+const student21 = document.getElementById("student-21");
+const student22 = document.getElementById("student-22");
+const student23 = document.getElementById("student-23");
+const student24 = document.getElementById("student-24");
+const student25 = document.getElementById("student-25");
+const student26 = document.getElementById("student-26");
+const student27 = document.getElementById("student-27");
+const student28 = document.getElementById("student-28");
+const student29 = document.getElementById("student-29");
+const student30 = document.getElementById("student-30");
+const student31 = document.getElementById("student-31");
+const student32 = document.getElementById("student-32");
+const student33 = document.getElementById("student-33");
+const student34 = document.getElementById("student-34");
+const student35 = document.getElementById("student-35");
+const student36 = document.getElementById("student-36");
+const student37 = document.getElementById("student-37");
+const item0 = document.getElementById("item_0");
+const item1 = document.getElementById("item_1");
+const item2 = document.getElementById("item_2");
+const item3 = document.getElementById("item_3");
+const item4 = document.getElementById("item_4");
+const item5 = document.getElementById("item_5");
+const item6 = document.getElementById("item_6");
+const item7 = document.getElementById("item_7");
+const item8 = document.getElementById("item_8");
+const item9 = document.getElementById("item_9");
+const item10 = document.getElementById("item_10");
+const item11 = document.getElementById("item_11");
+const item12 = document.getElementById("item_12");
+const item13 = document.getElementById("item_13");
+const item14 = document.getElementById("item_14");
+const item15 = document.getElementById("item_15");
+const item16 = document.getElementById("item_16");
+const item17 = document.getElementById("item_17");
+const item18 = document.getElementById("item_18");
+const item19 = document.getElementById("item_19");
+const item20 = document.getElementById("item_20");
+const item21 = document.getElementById("item_21");
+const item22 = document.getElementById("item_22");
+const item23 = document.getElementById("item_23");
+const item24 = document.getElementById("item_24");
+const item25 = document.getElementById("item_25");
+const item26 = document.getElementById("item_26");
+const item27 = document.getElementById("item_27");
+const item28 = document.getElementById("item_28");
+const item29 = document.getElementById("item_29");
+const item30 = document.getElementById("item_30");
+const item31 = document.getElementById("item_31");
+const item32 = document.getElementById("item_32");
+const item33 = document.getElementById("item_33");
+const item34 = document.getElementById("item_34");
+const item35 = document.getElementById("item_35");
+const item36 = document.getElementById("item_36");
+const item37 = document.getElementById("item_37");
+const active = document.querySelector(".active");
+function d0() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item0.classList.add("active");
+    }
+  }
+}
+function d1() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item1.classList.add("active");
+    }
+  }
+}
+function d2() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item2.classList.add("active");
+    }
+  }
+}
+function d3() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item3.classList.add("active");
+    }
+  }
+}
+function d4() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item4.classList.add("active");
+    }
+  }
+}
+function d5() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item5.classList.add("active");
+    }
+  }
+}
+function d6() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item6.classList.add("active");
+    }
+  }
+}
+function d7() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item7.classList.add("active");
+    }
+  }
+}
+function d8() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item8.classList.add("active");
+    }
+  }
+}
+function d9() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item9.classList.add("active");
+    }
+  }
+}
+function d10() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item10.classList.add("active");
+    }
+  }
+}
+function d11() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item11.classList.add("active");
+    }
+  }
+}
+function d12() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item12.classList.add("active");
+    }
+  }
+}
+function d13() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item13.classList.add("active");
+    }
+  }
+}
+function d14() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item14.classList.add("active");
+    }
+  }
+}
+function d15() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item15.classList.add("active");
+    }
+  }
+}
+function d16() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item16.classList.add("active");
+    }
+  }
+}
+function d17() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item17.classList.add("active");
+    }
+  }
+}
+function d18() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item18.classList.add("active");
+    }
+  }
+}
+function d19() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item19.classList.add("active");
+    }
+  }
+}
+function d20() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item20.classList.add("active");
+    }
+  }
+}
+function d21() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item21.classList.add("active");
+    }
+  }
+}
+function d22() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item22.classList.add("active");
+    }
+  }
+}
+function d23() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item23.classList.add("active");
+    }
+  }
+}
+function d24() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item24.classList.add("active");
+    }
+  }
+}
+function d25() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item25.classList.add("active");
+    }
+  }
+}
+function d26() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item26.classList.add("active");
+    }
+  }
+}
+function d27() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item27.classList.add("active");
+    }
+  }
+}
+function d28() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item28.classList.add("active");
+    }
+  }
+}
+function d29() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item29.classList.add("active");
+    }
+  }
+}
+function d30() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item30.classList.add("active");
+    }
+  }
+}
+function d31() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item31.classList.add("active");
+    }
+  }
+}
+function d32() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item32.classList.add("active");
+    }
+  }
+}
+function d33() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item33.classList.add("active");
+    }
+  }
+}
+function d34() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item34.classList.add("active");
+    }
+  }
+}
+function d35() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item35.classList.add("active");
+    }
+  }
+}
+function d36() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item36.classList.add("active");
+    }
+  }
+}
+function d37() {
+  if (active) {
+    for (const item of items) {
+      item.classList.remove("active");
+      item37.classList.add("active");
+    }
+  }
+}
+student0.addEventListener("click", d0);
+student1.addEventListener("click", d1);
+student2.addEventListener("click", d2);
+student3.addEventListener("click", d3);
+student4.addEventListener("click", d4);
+student5.addEventListener("click", d5);
+student6.addEventListener("click", d6);
+student7.addEventListener("click", d7);
+student8.addEventListener("click", d8);
+student9.addEventListener("click", d9);
+student10.addEventListener("click", d10);
+student11.addEventListener("click", d11);
+student12.addEventListener("click", d12);
+student13.addEventListener("click", d13);
+student14.addEventListener("click", d14);
+student15.addEventListener("click", d15);
+student16.addEventListener("click", d16);
+student17.addEventListener("click", d17);
+student18.addEventListener("click", d18);
+student19.addEventListener("click", d19);
+student20.addEventListener("click", d20);
+student21.addEventListener("click", d21);
+student22.addEventListener("click", d22);
+student23.addEventListener("click", d23);
+student24.addEventListener("click", d24);
+student25.addEventListener("click", d25);
+student26.addEventListener("click", d26);
+student27.addEventListener("click", d27);
+student28.addEventListener("click", d28);
+student29.addEventListener("click", d29);
+student30.addEventListener("click", d30);
+student31.addEventListener("click", d31);
+student32.addEventListener("click", d32);
+student33.addEventListener("click", d33);
+student34.addEventListener("click", d34);
+student35.addEventListener("click", d35);
+student36.addEventListener("click", d36);
+student37.addEventListener("click", d37);
